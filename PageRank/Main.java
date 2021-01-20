74e0067b9928dad21740a4b5ba26231e8304dc22
ExecutableMarley <47697856+ExecutableMarley@users.noreply.github.com>
2021-01-20 09:39:23 +0100

    Code refactoring
    Output looks now cleaner and the decimal precision can be adjusted. The start matrix is no longer hardcoded into the class and can be dynamically set

--- PageRank/Main.java
+++ PageRank/Main.java
@@ -27,144 +27,17 @@ package com.company;
 //
 class MarkovKette
 {
-    int maxMatrixIndex = 8;
-    //Self explaining
-    double adjazenmatrix[][] =
-            { //
-                    // 0 1 2 3 4 5 6 7
-                    {0,0,0,0,0,1,0,0}, //0
-                    {1,0,0,1,0,0,0,0}, //1
-                    {1,1,0,0,0,0,0,1}, //2
-                    {0,1,0,0,0,1,1,0}, //3
-                    {0,0,1,1,0,0,0,0}, //4
-                    {0,0,0,0,0,0,1,0}, //5
-                    {0,0,1,0,1,0,0,1}, //6
-                    {0,0,0,0,0,0,0,0}, //7
-            };
+    //We get that from the constructor parameter
+    double adjazenmatrix[][];
     //Stuff we will calculate
     double tMatrix[][]; //Matrix T |
     double rMatrix[][]; //Matrix R | Random Jump Matrix
     double pMatrix[][]; //Matrix P | Übergangs Matrix
 
-
-    /*
-    Calculation Functions
-    */
-    //Calculate the Matrix t from the "adjazenmatrix" and the epsilon value
-    void calculateT_Matrix(double epsilon)
+    MarkovKette(double newMatrix[][])
     {
-        //Init tMatrix
-        tMatrix = new double[maxMatrixIndex][maxMatrixIndex];
-
-        double rowSum = 0;
-
-        for (int index_1 = 0; index_1 < maxMatrixIndex; index_1++)
-        {
-            rowSum = sumOfRow(adjazenmatrix[index_1], maxMatrixIndex);
-            for (int index_2 = 0; index_2 < maxMatrixIndex; index_2++)
-            {
-                if (rowSum > 0) //Ausgehende Kanten vorhanden an diesem knoten
-                    if (adjazenmatrix[index_1][index_2] > 0) //Vorhandene kante an diesem punkt
-                        tMatrix[index_1][index_2] = (adjazenmatrix[index_1][index_2] - epsilon) / rowSum;
-                    else //Keine Kante an diesem Punkt
-                        tMatrix[index_1][index_2] = 0;
-                else //Keine Ausgehenden Kanten vorhanden an diesem knoten -> Random Jump
-                    tMatrix[index_1][index_2] = (1 - epsilon) / maxMatrixIndex;
-            }
-        }
+        adjazenmatrix = newMatrix;
     }
-    //Calculate the R matrix
-    void calculateR_Matrix(double epsilon)
-    {
-        rMatrix = new double[maxMatrixIndex][maxMatrixIndex];
-
-        for (int index_1 = 0; index_1 < maxMatrixIndex; index_1++)
-        {
-            for (int index_2 = 0; index_2 < maxMatrixIndex; index_2++)
-            {   //B1g m4th right here:
-                rMatrix[index_1][index_2] = epsilon / maxMatrixIndex;
-            }
-        }
-    }
-    //Calculate the P matrix
-    void calculateP_Matrix()
-    {
-        pMatrix = new double[maxMatrixIndex][maxMatrixIndex];
-        for (int index_1 = 0; index_1 < maxMatrixIndex; index_1++)
-        {
-            for (int index_2 = 0; index_2 < maxMatrixIndex; index_2++)
-            {
-                pMatrix[index_1][index_2] = tMatrix[index_1][index_2]+rMatrix[index_1][index_2];
-            }
-        }
-    }
-    //This will do the power iteration and returns if it was successfull
-    boolean doPowerIteration(double startVektor[],double delta)
-    {
-        printString("Power-Iteration has started\n");
-        //This arrays holds the current and last vector calculated vector
-        double curArray[] = {0,0,0,0,0,0,0,0};
-        double lastArray[] = startVektor;
-        //We keep track of the iterations with this
-        int currentIteration = 0;
-
-        //curLine Stores the line we print
-        String curLine = "x(0) = (";
-        for (int i = 0; i < maxMatrixIndex;i++)
-        {
-            curLine = curLine + Double.toString(startVektor[i]) + " ";
-        }
-        curLine = curLine + ")";
-        printString(curLine);
-
-        //The for loop is only here to prevent an infinity loop in case something goes wrong
-        //10000 operations should never be reached and will save the programm from an infinity loop
-        for (int i = 0; i < 10000; i++)
-        {
-            currentIteration++;
-
-            //Calculate the current iteration vector
-            for (int index_1 = 0; index_1 < maxMatrixIndex; index_1++)
-            {
-                double newVal = 0;
-                for (int index_2 = 0; index_2 < maxMatrixIndex; index_2++)
-                {
-                    newVal = newVal + pMatrix[index_2][index_1] * lastArray[index_2];
-                }
-                //
-                curArray[index_1] = newVal;
-            }
-
-            //Assemble the next output line
-            curLine = "x(" + currentIteration + ") = (";
-            for (int ii = 0; ii < maxMatrixIndex;ii++)
-            {
-                curLine = curLine + Double.toString(curArray[ii]) + " ";
-            }
-            curLine = curLine + ")";
-            //Output current vector
-            printString(curLine);
-
-
-            //Break the loop if the difference is smaller then delta
-            if (delta > difOfArrays(curArray,lastArray,maxMatrixIndex))
-            {
-                //Return that the operation was successfull
-                printString("\nPower-Iteration Terminated after " + Integer.toString(currentIteration) + " iterations \n");
-                return true;
-            }
-
-            //Copy curArray into lastArray
-            for (int ii = 0; ii < maxMatrixIndex;ii++)
-            {
-                lastArray[ii] = curArray[ii];
-            }
-        }
-        //Something went wrong and the loop run more times than expected
-        printString("Power operation reached iteration limit and got Terminated after 10.000 iterations");
-        return false;
-    }
-
 
     //Return the sum of an array row
     double sumOfRow(double array[], int maxIndex)
@@ -186,6 +59,128 @@ class MarkovKette
         }
         return result;
     }
+    /*
+    Calculation Functions
+    */
+    //Calculate the Matrix t from the "adjazenmatrix" and the epsilon value
+    void calculateT_Matrix(double epsilon)
+    {
+        //Init tMatrix
+        tMatrix = new double[adjazenmatrix.length][adjazenmatrix[0].length];
+
+        double rowSum = 0;
+
+        for (int index_1 = 0; index_1 < adjazenmatrix.length; index_1++)
+        {
+            rowSum = sumOfRow(adjazenmatrix[index_1], adjazenmatrix[index_1].length);
+            for (int index_2 = 0; index_2 < adjazenmatrix[index_1].length; index_2++)
+            {
+                if (rowSum > 0) //Ausgehende Kanten vorhanden an diesem knoten
+                    if (adjazenmatrix[index_1][index_2] > 0) //Vorhandene kante an diesem punkt
+                        tMatrix[index_1][index_2] = (adjazenmatrix[index_1][index_2] - epsilon) / rowSum;
+                    else //Keine Kante an diesem Punkt
+                        tMatrix[index_1][index_2] = 0;
+                else //Keine Ausgehenden Kanten vorhanden an diesem knoten -> Random Jump
+                    tMatrix[index_1][index_2] = (1 - epsilon) / adjazenmatrix[index_2].length;
+            }
+        }
+    }
+    //Calculate the R matrix
+    void calculateR_Matrix(double epsilon)
+    {
+        rMatrix = new double[adjazenmatrix.length][adjazenmatrix[0].length];
+
+        for (int index_1 = 0; index_1 < adjazenmatrix.length; index_1++)
+        {
+            for (int index_2 = 0; index_2 < adjazenmatrix[index_1].length; index_2++)
+            {
+                rMatrix[index_1][index_2] = epsilon / adjazenmatrix[index_2].length;
+            }
+        }
+    }
+    //Calculate the P matrix
+    void calculateP_Matrix()
+    {
+        pMatrix = new double[adjazenmatrix.length][adjazenmatrix[0].length];
+        for (int index_1 = 0; index_1 < adjazenmatrix.length; index_1++)
+        {
+            for (int index_2 = 0; index_2 < adjazenmatrix[index_1].length; index_2++)
+            {
+                pMatrix[index_1][index_2] = tMatrix[index_1][index_2]+rMatrix[index_1][index_2];
+            }
+        }
+    }
+    //This will do the power iteration and returns if it was successfull
+    boolean doPowerIteration(double startVektor[],double delta, int precision)
+    {
+        printString("Power-Iteration has started\n");
+        //This arrays holds the current and last vector calculated vector
+        double curArray[] = new double[startVektor.length];
+        double lastArray[] = startVektor;
+        //We keep track of the iterations with this
+        int currentIteration = 0;
+
+        //curLine Stores the line we print
+        String curLine = "x(0) = (";
+        for (int i = 0; i < adjazenmatrix.length;i++)
+        {
+            curLine = curLine + String.format("%." + 10 + "f ", startVektor[i]);
+        }
+        curLine = curLine + ")";
+        printString(curLine);
+
+        //The for loop is only here to prevent an infinity loop in case something goes wrong
+        //10000 operations should never be reached and will save the programm from an infinity loop
+        for (int i = 0; i < 10000; i++)
+        {
+            currentIteration++;
+
+            //Calculate the current iteration vector
+            for (int index_1 = 0; index_1 < adjazenmatrix.length; index_1++)
+            {
+                double newVal = 0;
+                for (int index_2 = 0; index_2 < adjazenmatrix[index_1].length; index_2++)
+                {
+                    newVal = newVal + pMatrix[index_2][index_1] * lastArray[index_2];
+                }
+                //
+                curArray[index_1] = newVal;
+            }
+
+            //Assemble the next output line
+            curLine = "x(" + currentIteration + ") = (";
+            for (int ii = 0; ii < curArray.length;ii++)
+            {
+                curLine = curLine + String.format("%." + 10 + "f ", curArray[ii]);
+            }
+            curLine = curLine + ")";
+            //Output current vector
+            printString(curLine);
+
+
+            //Break the loop if the difference is smaller then delta
+            if (delta > difOfArrays(curArray,lastArray,curArray.length))
+            {
+                //Return that the operation was successfull
+                printString("\nPower-Iteration Terminated after " + Integer.toString(currentIteration) + " iterations \n");
+                return true;
+            }
+
+            //Copy curArray into lastArray
+            for (int ii = 0; ii < curArray.length;ii++)
+            {
+                lastArray[ii] = curArray[ii];
+            }
+        }
+        //Something went wrong and the loop run more times than expected
+        printString("Power operation reached iteration limit and got Terminated after 10.000 iterations");
+        return false;
+    }
+    //Do power iteration with a decimal precision of 10
+    boolean doPowerIteration(double startVektor[],double delta)
+    {
+        return doPowerIteration(startVektor,delta,10);
+    }
 
     //Print functions
     //Used to print our results into the console
@@ -194,20 +189,20 @@ class MarkovKette
         System.out.println(str);
     }
     //This function will print a 2d matrix to the console
-    void printMatrix(double Matrix[][], boolean castToInt)
+    void printMatrix(double Matrix[][], int precsion)
     {
         //String that we will output at the end
         String finalString = "";
 
-        for (int index_1 = 0; index_1 < maxMatrixIndex; index_1++)
+        //Format String with presision decimal places
+        String str = "%." + precsion + "f";
+
+        for (int index_1 = 0; index_1 < Matrix.length; index_1++)
         {
             finalString = finalString + "| ";
-            for (int index_2 = 0; index_2 < maxMatrixIndex; index_2++)
+            for (int index_2 = 0; index_2 < Matrix[index_1].length; index_2++)
             {
-                if (!castToInt)
-                    finalString = finalString + Double.toString(Matrix[index_1][index_2]) + " ";
-                else
-                    finalString = finalString + Integer.toString((int)Matrix[index_1][index_2]) + " ";
+                finalString = finalString + String.format(str, Matrix[index_1][index_2]) + " ";
             }
             finalString = finalString + " | \n";
         }
@@ -215,21 +210,37 @@ class MarkovKette
         printString(finalString);
     }
 
+    void printAdjazenmatrix(int precision)
+    {
+        printMatrix(adjazenmatrix, precision);
+    }
     void printAdjazenmatrix()
     {
-        printMatrix(adjazenmatrix, true);
+        printMatrix(adjazenmatrix, 0);
+    }
+    void printMatrixT(int precision)
+    {
+        printMatrix(tMatrix,precision);
     }
     void printMatrixT()
     {
-        printMatrix(tMatrix,false);
+        printMatrix(tMatrix,5);
+    }
+    void printMatrixR(int precision)
+    {
+        printMatrix(rMatrix,precision);
     }
     void printMatrixR()
     {
-        printMatrix(rMatrix,false);
+        printMatrix(rMatrix,5);
+    }
+    void printMatrixP(int precision)
+    {
+        printMatrix(pMatrix,precision);
     }
     void printMatrixP()
     {
-        printMatrix(pMatrix,false);
+        printMatrix(pMatrix,5);
     }
 }
 
@@ -243,41 +254,53 @@ public class Main
         //e von der Aufgabenstellung
         double epsilon = 0.6f;
 
-        MarkovKette markovKette = new MarkovKette();
+        //Adjazenmatrix von der aufgabenstellung
+        double adjazenmatrix[][] =
+                { //
+                        // 0 1 2 3 4 5 6 7
+                        {0,0,0,0,0,1,0,0}, //0
+                        {1,0,0,1,0,0,0,0}, //1
+                        {1,1,0,0,0,0,0,1}, //2
+                        {0,1,0,0,0,1,1,0}, //3
+                        {0,0,1,1,0,0,0,0}, //4
+                        {0,0,0,0,0,0,1,0}, //5
+                        {0,0,1,0,1,0,0,1}, //6
+                        {0,0,0,0,0,0,0,0}, //7
+                };
+
+        MarkovKette markovKette = new MarkovKette(adjazenmatrix);
+
 
         //markovKette.printString("Proccess Entry");
 
         //Adjazenzmatrix von G wird hier ausgegeben
         markovKette.printString("Adjazenmatrix wird ausgegeben:\n");
-        markovKette.printAdjazenmatrix();
+        markovKette.printAdjazenmatrix(0);
 
         //T Matrix berechnen mit epsilon als parameter
         markovKette.calculateT_Matrix(epsilon);
 
-        //Die Matrix wird jetzt etwas unschöner werden, da die aufgabenstellung keine rundungen erwähnt
-        //und das beispiel aus der vorlesung auch nicht gerundet war, werde ich das hier auch nicht tun
-        //(abgesehen das es pseudo gerundet wird durch die float precision)
         markovKette.printString("Matrix T wird ausgegeben\n");
-        markovKette.printMatrixT();
+        markovKette.printMatrixT(5);
 
-        //Ich denke es sollte selbsterklärend sein was die folgenden statements machen
-        //Die namen der methoden sollten relativ aussagekräftig sein
+
+
         markovKette.calculateR_Matrix(epsilon);
 
         markovKette.printString("Matrix R wird ausgegeben\n");
-        markovKette.printMatrixR();
+        markovKette.printMatrixR(5);
 
 
         markovKette.calculateP_Matrix();
         markovKette.printString("Matrix P wird ausgegeben\n");
-        markovKette.printMatrixP();
+        markovKette.printMatrixP(5);
 
         //Die parameter aus der aufgabenstellung
         double startVektor[] = {0.1f,0.05f,0.13f,0.08f,0.04f,0.06f,0.24f,0.3f};
         double delta = 0.001f;
 
         //Führt die power itaration durch und schreibt die ergebnisse in die konsole
-        boolean powerIterationResult = markovKette.doPowerIteration(startVektor, delta);
-
+        //Last parameter is the decimal precision
+        boolean powerIterationResult = markovKette.doPowerIteration(startVektor, delta, 10);
     }
 }
