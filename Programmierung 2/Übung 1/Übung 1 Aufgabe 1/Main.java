33510ef8cc2913241561484fd4f9fbb4c844bc8d
Marley Arns <47697856+ExecutableMarley@users.noreply.github.com>
2021-04-13 16:37:56 +0200

    Create Main.java

---
+++ Programmierung 2/Übung 1/Übung 1 Aufgabe 1/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+final class Main
+{
+    private Main() {}
+
+    public static void main(String[] args)
+    {
+        javafx.application.Application.launch(FunctionCurve.class, args);
+    }
+}
