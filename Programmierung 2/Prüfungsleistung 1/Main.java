cafe87e5504c96832bff4e87e442b95563c9eb05
Marley Arns <47697856+ExecutableMarley@users.noreply.github.com>
2021-04-27 17:19:59 +0200

    Create Main.java

---
+++ Programmierung 2/Prüfungsleistung 1/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        if (true)
+        {
+            javafx.application.Application.launch(A1.class, args);
+        }
+        else
+        {
+            javafx.application.Application.launch(A2.class, args);
+        }
+    }
+}
