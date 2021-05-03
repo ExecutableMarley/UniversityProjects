2743958309445f47a6d96da7d52bc542ca405ee1
Marley Arns <47697856+ExecutableMarley@users.noreply.github.com>
2021-05-03 19:27:26 +0200

    Update Main.java

--- Programmierung 2/Übung 4/Main.java
+++ Programmierung 2/Übung 4/Main.java
@@ -1,11 +1,48 @@
 package com.company;
 
-final class Main
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Main
 {
-    private Main() {}
 
     public static void main(String[] args)
     {
-        javafx.application.Application.launch(FunctionCurve.class, args);
+	// write your code here
+
+        Random rand = new Random(System.currentTimeMillis());
+
+        List <Integer> intListe = new LinkedList<Integer>();
+
+        for (int i = 0; i < 10; i++)
+            intListe.add(rand.nextInt(1000));
+
+        List <String> stringListe = new LinkedList<String>();
+
+        stringListe.add("A test");
+        stringListe.add("B test");
+        stringListe.add("abc");
+
+
+        System.out.println(intListe.toString());
+
+
+
+        //Aufgabe 1 (a)
+        Double average = ListUtils.average(intListe);
+
+        System.out.printf("Average Value of even number = [%f] \n",average);
+
+        //Aufgabe 1 (b)
+        List <String> stringresult = ListUtils.upperCase(stringListe);
+
+        System.out.println(stringresult.toString());
+
+
+        //Aufgabe 1 (c)
+        String idontevenknow = ListUtils.getString(intListe);
+
+        System.out.println(idontevenknow);
     }
 }
