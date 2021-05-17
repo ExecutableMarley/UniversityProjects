1a247a52595d9fa89af0aaf794fd97c3fd477f54
Marley Arns <47697856+ExecutableMarley@users.noreply.github.com>
2021-05-17 22:40:39 +0200

    Fixed mistake

--- Programmierung 2/Übung 4/Main.java
+++ Programmierung 2/Übung 4/Main.java
@@ -1,134 +1,90 @@
 package com.company;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Random;
+import java.util.function.Function;
 
-class ListUtils
+public class Main
 {
-    /*
-    *This Constructor prevents:
-    * 1. that a subclass is created that extends this class
-    * 2. that an instance can be created from this class
-    *
-    * Why?: Because the sole constructor is explicitly private.
-    * Subclass therefore subclasses can't use super() and an instance cant be created
-    * Also the default constructors is only generated if there is no other constructor
-     */
-    private ListUtils(){};
 
-    /**
-     * Returns the Arithmetic mean of the even numbers from the provided Integer list
-     * @param list The list that is used
-     * @return Arithmetic mean of all even numbers
-     */
-    public static double average(List<Integer> list)
+    public static void main(String[] args)
     {
-        return list.stream().filter(i -> i % 2 == 0).reduce(0, Integer::sum)
-                / (double)list.stream().filter(i -> i % 2 == 0).count();
-    }
+        Random rand = new Random(System.currentTimeMillis());
 
-    /**
-     * Transforms all strings that either starts with an "a"/"A" or have an size that equals 3 to uppercase
-     * @param list The list that is used
-     * @return the transformed list of strings
-     */
-    public static List<String> upperCase(List<String> list)
-    {
-        /*
-        * Step 1: Convert to stream
-        * Step 2: Modify strings that fulfill condition to upper case variant
-        * Step 3: Convert to list again
-        * */
-        return  list.stream().
-                map(s -> (s.startsWith("A") || s.startsWith("a") || s.length() == 3) ? s.toUpperCase() : s).
-                        collect(Collectors.toList());
-    }
+        List <Integer> intListe = new LinkedList<Integer>();
 
-    /**
-     * Converts an List of integers to and list of strings where each strings starts with
-     * either an g (for even numbers) or and u (for odd numbers)
-     * @param list The list that is used
-     * @return the converted list of strings
-     */
-   public static String getString(List<Integer> list)
-    {
-        return list.stream().
-                map(integer -> String.format( (integer % 2 == 0 ? "g" : "u") + "%d, ", integer)).
-                reduce("", String::concat);
-    }
-}
+        for (int i = 0; i < 10; i++)
+            intListe.add(rand.nextInt(1000));
+
+        List <String> stringListe = new LinkedList<String>();
+
+        stringListe.add("A test");
+        stringListe.add("B test");
+        stringListe.add("abc");
+
+        System.out.println(intListe.toString() + "\n");
 
 
-/*
-* Beispiel: Betrachtete Zeichenkette "Hallo Trier "
-→ 5-Gramme sind: {"Hallo", "allo ", "llo T", "lo Tr", "o Tri", " Trie", "Trier"}.
-* */
 
-class NGramme
-{
-    /**
-     *
-     * */
-    static public List<String> someFunction(String param, int length)
-    {
-        if (length > param.length())
-            return null;
+        //Aufgabe 1 a
+        System.out.println("Exercise 1 (a):");
 
-        //Normalize string
-        String nString = param.trim().replaceAll("\\s+", " ");
+        Double average = ListUtils.average(intListe);
 
-        /*
-        * Step 1: generate sequence of number 0,1,2,3,4....
-        * Step 2: Limit the resulting stream to string.length + 1 - length elements
-        * Step 3: Convert those number sequences n-Gramme (substrings of source string)
-        * Step 4: Convert to string list and return
-        * */
-        return Stream.iterate(0, n -> n + 1).
-                limit(nString.length() + 1 - length).
-                map(i -> nString.substring(i, i + length)).
-                collect(Collectors.toList());
-    }
+        System.out.printf("Average Value of even number = [%f] \n\n",average);
 
-    /**
-     *
-     * */
-    static public void someFunction(List<String> param)
-    {
-        //The map that will safe the count of all possible n-grams
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        //Aufgabe 1 b
+        System.out.println("Exercise 1 (b):");
 
-        for (String curString : param)
-        {
-            //Normalize string
-            curString = curString.trim().replaceAll("\\s+", " ");
+        List <String> stringresult = ListUtils.upperCase(stringListe);
 
-            for (int i = 1; i <= curString.length(); i++)
-            {
-                //Get all n-grams of the specified size
-                List<String> gramList = someFunction(curString, i);
+        System.out.println(stringresult.toString()+ "\n");
 
-                for (String curGram : gramList)
-                {
-                    //Check if we already have the current gram listed
-                    if (map.containsKey(curGram))
-                    {
-                        //Increase entry by one
-                        map.put(curGram, map.get(curGram) + 1);
-                    }
-                    //Otherwise we create an entry
-                    else
-                    {
-                        map.put(curGram, 1);
-                    }
-                }
-            }
-        }
 
-        map.forEach((str, i) ->
-                System.out.printf("N-Gram: [%s] Count: [%d]\n", str,i));
+        //Aufgabe 1 c
+        System.out.println("Exercise 1 (c):\n");
 
-        System.out.println();
+        String idontevenknow = ListUtils.getString(intListe);
+
+        System.out.println(idontevenknow + "\n");
+
+
+        //Aufgabe 2
+        System.out.println("Exercise 2:\n");
+
+        NGramme.someFunction("Hallo   Trier ", 5).forEach(System.out::println);
+
+        List<String> stringList = Arrays.asList("Hallo   Trier ", "  Hallo    World  ");
+
+        NGramme.someFunction(stringList);
+
+
+        //Aufgabe 3
+        System.out.println("Exercise 3:\n");
+
+        DataStructure dataStruct = new DataStructure();
+
+        EvenIteratorStrategy even = new EvenIteratorStrategy();
+
+        OddIteratorStrategy odd = new OddIteratorStrategy();
+
+        //Original printEven
+        dataStruct.printEven();
+
+        //Print method with printEven functionality
+        dataStruct.print(even);
+
+        //Print method with odd iterator
+        dataStruct.print(odd);
+
+        //Print with lambda function
+        dataStruct.print((Function<Integer, Boolean>) i -> i % 2 == 0);
+        dataStruct.print((Function<Integer, Boolean>) i -> i % 2 == 1);
+
+        //Print with method reference
+        dataStruct.print(DataStructure::isEvenIndex);
+        dataStruct.print(DataStructure::isOddIndex);
     }
 }
