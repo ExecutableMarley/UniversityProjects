674becc360530e6e906f76d6bb79d8043858d22f
Marley Arns <47697856+ExecutableMarley@users.noreply.github.com>
2021-07-10 15:59:13 +0200

    Fixed isses with mutlipe key word args

--- Programmierung 2/Übung 11/Dekorator.py
+++ Programmierung 2/Übung 11/Dekorator.py
@@ -1,11 +1,11 @@
 def printParams(func):
     def hull(*val, **kvals):
         # tuples of variable names starting with parameters
-        varnames = func.__code__.co_varnames
+        varNames = func.__code__.co_varnames
 
         # iterate through non named arguments
         for i in range(len(val)):
-            print("Parameter [" + varnames[i] + "] = " + str(val[i]))
+            print("Parameter [" + varNames[i] + "] = " + str(val[i]))
         # iterate through named arguments
         for kVal in kvals.items():
             print("Parameter [" + kVal[0] + "] = " + str(kVal[1]))
@@ -18,17 +18,17 @@ def printParams(func):
 
 
 @printParams
-def function1(x, y, z, keyArg = None):
+def function1(x, y, z, **keyArg):
     return x + y + z
 
 
-def function2(x, y, z, keyArg = None):
+def function2(x, y, z, **keyArg):
     return x + y + z
 
 
 # Decorations done at compile time
 print("Calling function 1:")
-function1(10, 20, 30, keyArg="SomeText")
+function1(10, 20, 30, keyArg="SomeText", someMore="123")
 
 # function2 before runtime decoration
 function2(10, 20, 30, keyArg="SomeText")
@@ -38,4 +38,4 @@ function2 = printParams(function2)
 
 # function2 after runtime decoration
 print("Calling function 2:")
-function2(10, 20, 30, keyArg="SomeText")
+function2(10, 20, 30, keyArg="SomeText", someMore="123")
