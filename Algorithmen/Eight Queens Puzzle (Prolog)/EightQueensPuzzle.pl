d7deeaec9eccbd7c9b2707ba46d9c457118f90f9
Marley Arns <47697856+ExecutableMarley@users.noreply.github.com>
2022-01-17 16:35:02 +0100

    Introduced new not_angreifen statement.

--- Algorithmen/Eight Queens Puzzle (Prolog)/EightQueensPuzzle.pl
+++ Algorithmen/Eight Queens Puzzle (Prolog)/EightQueensPuzzle.pl
@@ -9,26 +9,33 @@ permutation(Xs,[Z|Zs]) :- select(Z,Xs,Ys), permutation(Ys,Zs).
 permutation([],[]).
 
 %%Terminate Empty List
-angreifen(_,[],_).
+not_angreifen(_,[],_).
 
 %%Process List
-angreifen(X, [Head|T], Counter) :-
+not_angreifen(X, [Head|T], Counter) :-
     %%abs(Head - X) =\= Counter,
 	Head - X =\= Counter,
 	X - Head =\= Counter,
 	Counter2 is Counter + 1,
-	angreifen(X, T, Counter2).
+	not_angreifen(X, T, Counter2).
 
-%%angreifen entry point
-angreifen(X, Xs) :- 
-    angreifen(X, Xs, 1).
+%%no_angreifen entry point
+not_angreifen(X, Xs) :- 
+    not_angreifen(X, Xs, 1).
+
+%% Evaluates to the opposite of no_angreifen
+angreifen(X, Xs) :-
+    not(not_angreifen(X,Xs)).
+
+angreifen(X, Xs, C) :-
+    not(not_angreifen(X,Xs, C)).
 
 %%Terminate Empty List
 sicher([]).
 %%Process List
 sicher([Q|Qs]) :-
 	sicher(Qs),
-	angreifen(Q,Qs,1).
+	not_angreifen(Q,Qs, 1).
 
 %%Generate Solutions, N specifies the size of the board. Qs holds the generated results
 damen(N, Qs) :-
